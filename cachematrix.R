a7c25d9b0ea14da3b470977fb56b1b0002b0b879
surjya <padhy.surya@gmail.com>
2014-07-11 13:55:11 +0800

    The final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function:makeCacheMatrix - it creates an object of type list with the following functions
+## 1. makeCacheMatrix$set which set the matrix with input data as matrix
+## 2. makeCacheMatrix$set which gets the value of matrix inside
+## 3. makeCacheMatrix$setinv which sets inverted matrix
+## 4. makeCacheMatrix$getinv which gets inverted matrix inside 
 
 makeCacheMatrix <- function(x = matrix()) {
   invmat <- NULL
@@ -18,7 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function:cacheSolve - This function makes takes an object of type list which is output 
+## of above function. If its a new matrix then the inverted matrix is calculated newly. If
+## its the existing matrix data and inverted matrix is already calculate, it returns the 
+## cached inverted matrix. It saves time for repeated work of calculating inverted matrix
+## for same matrix data.
 
 cacheSolve <- function(x, ...) {
   invmat <- x$getinv()
